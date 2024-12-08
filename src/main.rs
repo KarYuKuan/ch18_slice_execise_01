b40627b51dae5cd0947d0cc9239dc558bc36c0e0
Samurai_Chia <jyjzone@163.com>
2024-12-08 15:54:30 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello_devil");
+
+    let s1 = &s[0..5];
+    let s2 = &s[5..10];
+
+    println!("-------the value of s1={s1}---------");
+    println!("-------the value of s1={s2}---------");
 }
